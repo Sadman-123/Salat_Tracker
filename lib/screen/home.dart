17d47001093d65e0bc11681895fcd74dcf9a12e5
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-10-22 22:35:29 +0600

    some changess

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -7,45 +7,45 @@ import 'package:salah_app/screen/home_components/didyoupraymsg.dart';
 import 'package:salah_app/screen/home_components/otherparts.dart';
 import 'package:salah_app/screen/home_components/prayer_card.dart';
 import 'package:salah_app/screen/home_components/salat_times.dart';
-class Home extends StatelessWidget{
-  SalatTimeController salat=Get.find();
+class Home extends StatelessWidget {
+  final SalatTimeController salat = Get.find();
   @override
   Widget build(BuildContext context) {
     var mdw = MediaQuery.sizeOf(context).width;
     var mdh = MediaQuery.sizeOf(context).height;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Obx((){
-          if(salat.isLoading.value){
-            return Container(
-              alignment: Alignment.center,
-              child: Center(child: CircularProgressIndicator(),
-            ));
-          }
-          else{
-            return Container(
-              margin: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  SizedBox(height: mdh*0.056,),
-                  AppbarPart(mdw, mdh),
-                  PrayerCard(mdw: mdw, mdh: mdh),
-                  SalatTimes(mdw: mdw, mdh: mdh),
-                  Didyoupraymsg(mdw: mdw, mdh: mdh),
-                  SizedBox(height: mdh*0.0025,),
-                  Otherparts(mdw: mdw, mdh: mdh),
-                  Hadithcard(mdw: mdw, mdh: mdh)
-                ],
-              ),
-            );
-          }
-        }),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home),label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.message),label: "Message"),
-          BottomNavigationBarItem(icon: Icon(Icons.person),label: "Profile"),
+     body:  SingleChildScrollView(
+       child: Obx(() {
+         if (salat.isLoading.value) {
+           return Container(
+             alignment: Alignment.center,
+             child: Center(child: CircularProgressIndicator()),
+           );
+         } else {
+           return Container(
+             margin: EdgeInsets.all(10),
+             child: Column(
+               children: [
+                 SizedBox(height: mdh * 0.056),
+                 AppbarPart(mdw, mdh),
+                 PrayerCard(mdw: mdw, mdh: mdh),
+                 SalatTimes(mdw: mdw, mdh: mdh),
+                 Didyoupraymsg(mdw: mdw, mdh: mdh),
+                 SizedBox(height: mdh * 0.0025),
+                 Otherparts(mdw: mdw, mdh: mdh),
+                 Hadithcard(mdw: mdw, mdh: mdh),
+               ],
+             ),
+           );
+         }
+       }),
+     ),
+      bottomNavigationBar: NavigationBar(
+        destinations: [
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.track_changes), label: "Tracker"),
+          NavigationDestination(icon: Icon(Icons.library_books), label: "Books"),
+          NavigationDestination(icon: Icon(Icons.settings), label: "Settings"),
         ],
       ),
     );
