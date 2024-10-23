cfa3a0f8bf4be9831e07a2f59b0c05fa56283278
Sadman-123 <19201103123@cse.bubt.edu.bd>
2024-10-23 19:42:51 +0600

    some changes in snackbar

--- lib/controller/user_controller.dart
+++ lib/controller/user_controller.dart
@@ -9,43 +9,51 @@ class UserController extends GetxController{
   TextEditingController regpass=TextEditingController();
   Future<void> signup()async
   {
-    try{
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(email: regemail.text, password: regpass.text);
-      Fluttertoast.showToast(
-          msg: "Created Successfully",
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.SNACKBAR,
-          backgroundColor: Colors.green.shade400,
-          textColor: Colors.white,
-          fontSize: 14.0
-      );
-      regemail.clear();
-      regpass.clear();
-    }on FirebaseAuthException catch(e)
-    {
-      String msg="";
-      if(e.code=='weak-password'){
-        msg="Weak Password";
-      }
-      else if(e.code=='email-already-in-use'){
-        msg="Account Exists!!";
-      }
-      Fluttertoast.showToast(
-          msg: msg,
-          toastLength: Toast.LENGTH_LONG,
-          gravity: ToastGravity.SNACKBAR,
-          backgroundColor: Colors.black54,
-          textColor: Colors.white,
-          fontSize: 14.0
-      );
-      regemail.clear();
-      regpass.clear();
-    }
-    catch(e){
-      print(e);
-    }
+   if(regemail.text.isEmpty || regpass.text.isEmpty)
+     {
+       custom_Toast("Please Fill Every Fields", false);
+       return;
+     }
+   else{
+     try{
+       await FirebaseAuth.instance.createUserWithEmailAndPassword(email: regemail.text, password: regpass.text);
+       custom_Toast("Created Successfully",true);
+       clear_fields();
+     }on FirebaseAuthException catch(e)
+     {
+       String msg="";
+       if(e.code=='weak-password'){
+         msg="Weak Password";
+       }
+       else if(e.code=='email-already-in-use'){
+         msg="Account Exists!!";
+       }
+       custom_Toast(msg,false);
+       clear_fields();
+     }
+     catch(e){
+       print(e);
+     }
+   }
   }
-
+  void custom_Toast(String msg,bool vl)
+  {
+      Fluttertoast.showToast(
+        msg: msg,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.SNACKBAR,
+        backgroundColor:vl?Colors.green.shade500: Color(0xFF161f28),
+        textColor: vl?Colors.white:Color(0xFFf7f6f7),
+        fontSize: 14.0,
+      );
+  }
+  void clear_fields()
+  {
+    loginmail.clear();
+    loginpass.clear();
+    regemail.clear();
+    regpass.clear();
+  }
   @override
   void dispose() {
     super.dispose();
